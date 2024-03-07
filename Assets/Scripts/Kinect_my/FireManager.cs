ced8c88726464621f63e2fa72feb94edc54d30b5
duolanda <duolanda@live.cn>
2024-03-07 19:37:19 +0800

    增加灭火倒计时和计数

--- Assets/Scripts/Kinect_my/FireManager.cs
+++ Assets/Scripts/Kinect_my/FireManager.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FireManager : MonoBehaviour
 {
     public GameObject firePrefab;
     public float spawnInterval = 0.5f; // 设置生成间隔为0.5秒
-    public float duration = 60f; // 设置持续时间为60秒
+    public float duration = 30f; // 设置持续时间为60秒
+    public TextMeshProUGUI textCountDownSecond;
+    public TextMeshProUGUI textExtinguishedCount;
+
     private Vector2 fireSize;
+    private int extinguishedCount = 0; // 扑灭的火焰数量
+
+
 
     void Start()
     {
 
-        fireSize = firePrefab.GetComponent<SpriteRenderer>().bounds.size;
-        
+        fireSize = firePrefab.GetComponent<SpriteRenderer>().bounds.size; 
         StartCoroutine(SpawnFires());
     }
 
@@ -32,19 +38,24 @@ public class FireManager : MonoBehaviour
             Random.Range(-spawnRangeY, spawnRangeY)
         );
 
-        Instantiate(firePrefab, spawnPosition, Quaternion.identity, transform);
+        GameObject fire = Instantiate(firePrefab, spawnPosition, Quaternion.identity, transform);
+        fire.GetComponent<FireTrigger>().OnExtinguished += () => extinguishedCount++; // 订阅事件
     }
     
 
     IEnumerator SpawnFires()
     {
-        float timer = 0f;
+        float timer = duration;
 
-        while (timer < duration)
+        while (timer > 0)
         {
             SpawnFire();
-            yield return new WaitForSeconds(spawnInterval); // 等待0.5秒
-            timer += spawnInterval;
+            yield return new WaitForSeconds(spawnInterval);
+            timer -= spawnInterval;
+            textCountDownSecond.text = Mathf.CeilToInt(timer) + "s"; // 更新倒计时文本
+            textExtinguishedCount.text = extinguishedCount.ToString(); // 更新扑灭火焰数量文本
         }
+
+        textCountDownSecond.text = "0";
     }
 }
--- Assets/Scripts/Kinect_my/FireTrigger.cs
+++ Assets/Scripts/Kinect_my/FireTrigger.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireTrigger : MonoBehaviour
 {
-	void OnTriggerEnter2D(Collider2D collider)
+    public event Action OnExtinguished; // 用来触发 UI 计数
+
+    void OnTriggerEnter2D(Collider2D collider)
 	{
-		//Debug.Log("成功触发");
-		Destroy(gameObject);
+        //Debug.Log("成功触发");
+        OnExtinguished?.Invoke(); // 触发事件
+        Destroy(gameObject);
 	}
 }
