0aa786a5e24ba973928e01d42a28246a31edc84f
duolanda <duolanda@live.cn>
2024-03-15 19:22:36 +0800

    新玩家加入退出颜色和光标颜色一致

--- Assets/Scripts/Kinect_my/UserUIManager.cs
+++ Assets/Scripts/Kinect_my/UserUIManager.cs
@@ -11,8 +11,18 @@ public class UserUIManager : MonoBehaviour
 
     private KinectManager km;
 
+    private Color32[] colors = new Color32[6]; 
+
     void Start()
     {
+        colors[0] = new Color32(217, 112, 99, 255); //红
+        colors[1] = new Color32(106, 130, 231, 255); //蓝
+        colors[2] = new Color32(112, 164, 67, 255); //绿
+        colors[3] = new Color32(238, 237, 90, 255); //黄
+        colors[4] = new Color32(220, 154, 66, 255); //橙
+        colors[5] = new Color32(141, 100, 161, 255); //紫
+
+
         GameObject KinectController = GameObject.Find("KinectController");
         if (KinectController != null)
         {
@@ -21,11 +31,15 @@ public class UserUIManager : MonoBehaviour
 
         km.OnUserAdded.AddListener(UserAdd);
         km.OnUserRemoved.AddListener(UserRemove);
+
+        userChangeText.overrideColorTags = true;
     }
 
     private void UserAdd(long userId, int userIndex)
     {
         userChangeText.text = "检测到游客加入";
+        userChangeText.color = colors[userIndex];
+
         userCountText.text = "当前操纵游客数：" + km.GetUsersCount();
         StopAllCoroutines(); // 停止所有协程，以防之前的协程还在运行
         StartCoroutine(HideUserChangeText()); // 启动协程来隐藏文本
@@ -34,6 +48,8 @@ public class UserUIManager : MonoBehaviour
     private void UserRemove(long userId, int userIndex)
     {
         userChangeText.text = "检测到游客离开";
+        userChangeText.color = colors[userIndex];
+
         userCountText.text = "当前操纵游客数：" + km.GetUsersCount();
         StopAllCoroutines(); 
         StartCoroutine(HideUserChangeText()); 
